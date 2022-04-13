482cc29a508770e098ff7cb4595cedb00d4a62af
rakib <sadiasorna1000@gmail.com>
2022-04-13 11:26:08 +0600

    firebase init file added

---
+++ src/__auth_steps.js
@@ -0,0 +1,27 @@
+/**
+ * 1. create a new project console.google.com
+ * 2. npm install firebase
+ * 3. make firebase.init.js file in src folder and paste the documentation which coppied from sdk doc
+ * 4. firebase settings > Authentication > enable email and password auth
+ * 
+ */
+
+
+
+
+
+
+/**
+ * hosting steps 
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 3. firebase init
+ */
+
+
+/**
+ * github origin change 
+ * 1. For checking primary url  > git remote -v
+ * 2. for new url > git remote set-url origin https------
+ * 
+ */
